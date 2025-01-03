a10e7f5eae2bd92b70767486e445e6bf86d3f617
tomikorzu <tomykorzu@icloud.com>
2025-01-03 02:07:18 -0300

    feat: add socket.io config in client and server

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,3 +1,16 @@
+import io from "socket.io-client";
+
+const socket = io("/");
+
 export default function Home() {
-  return <h1>HOME</h1>;
+  return (
+    <div>
+      <form className="">
+        <input type="text" placeholder="Enter your message" />
+        <button type="submit" className="text-extrabold text-2xl">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const serverHost = "http://localhost:3000";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/socket.io": {
+        target: serverHost,
+        ws: true,
+      },
+    },
+  },
+});
