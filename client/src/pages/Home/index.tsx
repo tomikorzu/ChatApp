350e8ed794e27f0295c7de277c2b8c40f5777bec
tomikorzu <tomykorzu@icloud.com>
2025-01-03 04:23:16 -0300

    feat(Home): use the SendMessage component

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,16 +1,12 @@
-import io from "socket.io-client";
-
-const socket = io("/");
+import SendMessageForm from "./SendMessageForm";
 
 export default function Home() {
   return (
-    <div>
-      <form className="">
-        <input type="text" placeholder="Enter your message" />
-        <button type="submit" className="">
-          Send
-        </button>
-      </form>
-    </div>
+    <>
+    <header></header>
+      <main className="h-screen flex justify-center items-center">
+        <SendMessageForm />
+      </main>
+    </>
   );
 }
