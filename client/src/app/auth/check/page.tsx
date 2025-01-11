bd71e6b017cd38e2dffef35ddddaf5e035705008
tomikorzu <tomykorzu@icloud.com>
2025-01-11 05:18:50 -0300

    feat(Auth/Check): add this page Layout

--- client/src/app/auth/check/page.tsx
+++ client/src/app/auth/check/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import AuthContainer from "../components/AuthContainer";
+import AuthForm from "../components/AuthForm";
+import Logo from "../components/Logo";
+import TextInput from "../components/TextInput";
+import Title from "../components/Title";
+import SubmitBtn from "../components/SubmitBtn";
+import Redirect from "../components/Redirect";
+
 export default function CheckPage() {
-  return <h1>checkpage</h1>;
+  const [email, setEmail] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setLoading(true);
+    console.log(code);
+  }
+  return (
+    <AuthContainer>
+        <Redirect url={"back"}>Go Back</Redirect>
+      <AuthForm action={handleSubmit}>
+        <Logo />
+        <Title>Check your Email</Title>
+        <TextInput
+          placeholder="Enter your email"
+          id="Email"
+          action={setEmail}
+        />
+        <TextInput placeholder="Enter your code" id="Code" action={setCode} />
+        <SubmitBtn disabled={loading}>
+          {loading ? "Checking..." : "Check Email"}
+        </SubmitBtn>
+      </AuthForm>
+    </AuthContainer>
+  );
 }
--- client/src/app/auth/components/Redirect.tsx
+++ client/src/app/auth/components/Redirect.tsx
@@ -1,5 +1,6 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Redirect({
   url,
@@ -8,13 +9,26 @@ export default function Redirect({
   url: string;
   children: React.ReactNode;
 }) {
+  const router = useRouter();
   return (
-    <Link
-      className="absolute flex justify-center font-semibold items-center gap-1 right-6 top-5 px-4 py-2 rounded-full bg-[#27947c] lg:hover:brightness-110 transition duration-[.4s]"
-      href={url}
-    >
-      {children}
-      <ArrowRight className="size-5" />
-    </Link>
+    <>
+      {url !== "back" ? (
+        <Link
+          className="absolute flex justify-center font-semibold items-center gap-1 right-6 top-5 px-4 py-2 rounded-full bg-[#27947c] lg:hover:brightness-110 transition duration-[.4s]"
+          href={url}
+        >
+          {children}
+          <ArrowRight className="size-5" />
+        </Link>
+      ) : (
+        <button
+          className="absolute flex justify-center font-semibold items-center gap-1 left-6 top-5 px-4 py-2 rounded-full bg-[#27947c] lg:hover:brightness-110 transition duration-[.4s]"
+          onClick={() => router.back()}
+        >
+          <ArrowLeft className="size-5" />
+          {children}
+        </button>
+      )}
+    </>
   );
 }
