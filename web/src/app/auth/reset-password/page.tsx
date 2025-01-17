126619ee1df364f830c54268132160a0a267555d
tomikorzu <tomykorzu@icloud.com>
2025-01-17 01:24:50 -0300

    fix: change the script dev to next dev

--- web/src/app/auth/reset-password/page.tsx
+++ web/src/app/auth/reset-password/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import { useState } from "react";
 import AuthContainer from "../components/AuthContainer";
