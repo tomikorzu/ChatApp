0cf119fc478bf698a27873ac035a2888a2146311
tomikorzu <tomykorzu@icloud.com>
2025-01-18 01:07:31 -0300

    fix(socket): change the env socket variable

--- web/src/shared/lib/socket.ts
+++ web/src/shared/lib/socket.ts
@@ -4,7 +4,7 @@ let socket: SocketIOClient.Socket;
 
 export const getSocket = () => {
   if (!socket) {
-    const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:4000";
+    const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
     socket = io(SOCKET_URL);
   }
   return socket;
