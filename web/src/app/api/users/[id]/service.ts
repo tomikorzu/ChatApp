cfbeeddfb53ef23e3d03ffb6b9467cec0c309ee3
tomikorzu <tomykorzu@icloud.com>
2025-01-20 07:14:47 -0300

    feat(dashboard): add the layout friends list

--- web/src/app/api/users/[id]/service.ts
+++ web/src/app/api/users/[id]/service.ts
@@ -3,7 +3,7 @@ import db from "@/server/database/db";
 export async function getUserById(id: number) {
   return new Promise((resolve, reject) => {
     db.get(
-      `SELECT username, profile_picture, bio, email, created_at FROM users WHERE id = ? AND verified = 1`,
+      `SELECT id, username, profile_picture, bio, email, created_at FROM users WHERE id = ? AND verified = 1`,
       [id],
       (err, row) => {
         if (err) {
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,34 @@
 "use client";
 
 import { deleteCookie } from "@/shared/lib/cookies";
+import { useSession } from "@/shared/providers/SessionProvider";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface Friends {
+  id: number;
+  username: string;
+  profile_picture: string;
+  bio: string;
+  email: string;
+  created_at: string;
+}
 
 export default function DashboardPage() {
+  const [friends, setFriends] = useState<Friends[] | []>([]);
+  const session = useSession();
+
+  async function getFriends(id: number | undefined) {
+    const res = await fetch(`/api/friends/${id}`);
+    if (res.status === 200) {
+      const { friends } = await res.json();
+      setFriends(friends);
+    } else setFriends([]);
+  }
+  useEffect(() => {
+    getFriends(session?.user.id);
+  }, [session?.user.id]);
+
   const router = useRouter();
   function handleLogout() {
     deleteCookie("auth-token");
@@ -18,6 +43,23 @@ export default function DashboardPage() {
       >
         Log Out
       </button>
+      <ul>
+        {friends ? (
+          friends.map((friend: Friends) => {
+            return (
+              <li key={friend.id}>
+                {friend.profile_picture && (
+                  <img src={friend.profile_picture} alt={friend.username} />
+                )}
+                <h2>{friend.username}</h2>
+                <p>{friend.bio}</p>
+              </li>
+            );
+          })
+        ) : (
+          <p>No friends found</p>
+        )}
+      </ul>
     </>
   );
 }
