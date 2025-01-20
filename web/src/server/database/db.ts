64195ef79f602bdc2c01af1c3ceb95e0c7684f98
tomikorzu <tomykorzu@icloud.com>
2025-01-20 06:54:55 -0300

    feat(db): add more columns in users table

--- web/src/server/database/db.ts
+++ web/src/server/database/db.ts
@@ -13,6 +13,8 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
     username TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
+    profile_picture TEXT,
+    bio TEXT,
     code varchar(6),
     verified BOOLEAN DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -76,5 +78,37 @@ function cleanUpUnverifiedUsers() {
 }
 
 setInterval(cleanUpUnverifiedUsers, 900000);
+
+function addFriends() {
+  db.run(
+    `INSERT INTO friends (user1_id, user2_id, status) VALUES (1, 2, 'accepted'),(1,3,'accepted'),(1,4,'accepted'),(1,5,'accepted')`
+  );
+}
+function addUsers() {
+  db.run(
+    `INSERT INTO users (username, email, password, code, verified, profile_picture) VALUES ("tomikorzu", "tomykorzu@icloud.com", "$2b$10$dn4vRLWvxsqOc099D24x4OQeRJvAhbucy74QtCnQEPR4RGRlOk2jC", "-", 1, "https://avatars.githubusercontent.com/u/167248198?v=4"),("nicokorzu", "nkorzusehec@fi.uba.ar", "$2b$10$dn4vRLWvxsqOc099D24x4OQeRJvAhbucy74QtCnQEPR4RGRlOk2jC", "-", 1, "https://avatars.githubusercontent.com/u/99510894?v=4"), ("mileDimitroff","miledimitroff1@gmail.com", "$2b$10$dn4vRLWvxsqOc099D24x4OQeRJvAhbucy74QtCnQEPR4RGRlOk2jC", "-", 1, ""), ("silvi", "silvanaredolatti@hotmail.com", "$2b$10$dn4vRLWvxsqOc099D24x4OQeRJvAhbucy74QtCnQEPR4RGRlOk2jC", "-", 1, ""), ("charly", "korzuarq@gmail.com", "$2b$10$dn4vRLWvxsqOc099D24x4OQeRJvAhbucy74QtCnQEPR4RGRlOk2jC", "-", 1, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.instagram.com%2Fcharly_korzu%2F&psig=AOvVaw20GbEn1jABF3NjSSKuaHfz&ust=1737450812850000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPCilef6g4sDFQAAAAAdAAAAABAE")`
+  );
+}
+
+db.serialize(() => {
+  db.get(`SELECT COUNT(*) FROM friends`, (err, row: { "COUNT(*)": number }) => {
+    if (err) {
+      console.error("Error checking for friends", err);
+      return;
+    }
+    if (row["COUNT(*)"] === 0) {
+      addFriends();
+    }
+  });
+  db.get(`SELECT COUNT(*) FROM users`, (err, row: { "COUNT(*)": number }) => {
+    if (err) {
+      console.error("Error checking for users", err);
+      return;
+    }
+    if (row["COUNT(*)"] === 0) {
+      addUsers();
+    }
+  });
+});
 
 export default db;
