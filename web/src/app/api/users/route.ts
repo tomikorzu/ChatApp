23531b37b3276ef6034e0eeffb2710fd8544779e
tomikorzu <tomykorzu@icloud.com>
2025-01-22 04:38:11 -0300

    fix(users): change the getUsers service import

--- web/src/app/api/users/route.ts
+++ web/src/app/api/users/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse as res } from "next/server";
-import { getUsers } from "./services";
+import { getUsers } from "@/server/services/users/get";
 
 export async function GET() {
   try {
     const users = await getUsers();
+    if (!users) return res.json({ error: "Users not found" }, { status: 404 });
     return res.json({ users }, { status: 200 });
   } catch (error) {
     return res.json({ error }, { status: 500 });
