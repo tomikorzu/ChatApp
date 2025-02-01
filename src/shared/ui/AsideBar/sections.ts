cb87a6b0965c42cdda0eeaaef45db4c16afc6dca
Tomas Korzusehec <tomykorzu@icloud.com>
2025-02-01 20:40:29 -0300

    feat(AsideBar): add all sections of this component

---
+++ src/shared/ui/AsideBar/sections.ts
@@ -0,0 +1,29 @@
+export const sections = {
+  mainSections: [
+    {
+      name: "Channels",
+      icon: "fa-solid fa-hashtag",
+      url: "/",
+    },
+    {
+      name: "Direct Messages",
+      icon: "fa-solid fa-comment",
+      url: "/dm",
+    },
+    {
+      name: "Groups",
+      icon: "fa-solid fa-users",
+      url: "/groups",
+    },
+    {
+      name: "Notifications",
+      icon: "fa-solid fa-bell",
+      url: "/notifications",
+    },
+  ],
+  settings: {
+    name: "Settings",
+    icon: "fa-solid fa-cog",
+    url: "/settings",
+  },
+};
