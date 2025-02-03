82e7c8c59e9a926c5783f0a2e92e10368a695e7f
Tomas Korzusehec <tomykorzu@icloud.com>
2025-02-02 22:22:12 -0300

    feat(AsideBar/Link): add color

--- src/shared/ui/AsideBar/Link.tsx
+++ src/shared/ui/AsideBar/Link.tsx
@@ -2,21 +2,31 @@ import Link from "next/link";
 
 export default function Button({
   pathname,
-  name,
   url,
   icon,
 }: {
   pathname: string;
-  name: string;
   url: string;
   icon: string;
 }) {
+  const condition = (url: string, pathname: string) => {
+    if (url !== "/" && pathname.includes(url)) {
+      return "text-[#fff] bg-[#444]";
+    } else {
+      if (pathname === url && url === "/") {
+        return "text-[#fff] bg-[#444]";
+      } else {
+        return "text-[#b2b2b2] lg:hover:bg-[#ffffff10]";
+      }
+    }
+  };
   return (
     <Link
       href={url}
-      className={`${
-        pathname === url ? "text-[#fff] bg-[#444] pointer-events-none" : "text-[#b2b2b2]"
-      } rounded-lg size-12 lg:hover:bg-[#ffffff10] transition duration-[.4s] flex items-center justify-center text-xl`}
+      className={`${condition(
+        url,
+        pathname
+      )} rounded-lg size-12 transition duration-[.4s] flex items-center justify-center text-xl`}
     >
       <i className={icon}></i>
     </Link>
