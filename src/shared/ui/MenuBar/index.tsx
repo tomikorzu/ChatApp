ffca9b9601addce769de70a09c4305f976a8f408
Tomas Korzusehec <tomykorzu@icloud.com>
2025-02-02 22:24:13 -0300

    feat(MenuBar): use it when the pathname not contains /auth

--- src/shared/ui/MenuBar/index.tsx
+++ src/shared/ui/MenuBar/index.tsx
@@ -1,9 +1,23 @@
 "use client";
 
-import Dm from "./Dm";
+import Channels from "./Channels/";
+import Dm from "./Dm/";
+import Groups from "./Groups/";
+import Header from "./components/Header";
+import Notifications from "./Notifications/";
+import Settings from "./Settings/";
 import { usePathname } from "next/navigation";
-
 export default function MenuBar() {
   const pathname = usePathname();
-  return <>{pathname.includes("/dm") && <Dm />}</>;
+  return (
+    <>
+      {!pathname.includes('/auth') && <Header>
+        {pathname === "/" && <Channels />}
+        {pathname.includes("/dm") && <Dm />}
+        {pathname.includes("/groups") && <Groups />}
+        {pathname.includes("/notifications") && <Notifications />}
+        {pathname.includes("/settings") && <Settings />}
+      </Header>}
+    </>
+  );
 }
